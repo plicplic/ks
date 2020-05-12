3e315a9a602cb28d082f9db731213707cb81d315
Alain <alainj@gmail.com>
2020-05-12 13:20:37 +0200

    Testing Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
-export default () => (
-	<div style={{ color: 'teal' }}>
-	  <Header headerText="About Gatsby" />
-	  <Header headerText="It's pretty cool" />
-	  <p>Such now. Very React.</p>
-        </div>
-)
+export default function About() {
+  return (
+  <Layout>
+    <div>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
+    </div>
+  </Layout>
+  )
+}
